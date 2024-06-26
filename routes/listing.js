4340d88a9b31bb04fbd5454ccbb378729e4e8579
Teesha Dembla <teeshadembla0507@gmail.com>
2024-06-26 19:48:18 +0530

    resturctured code with express router

--- routes/listing.js
+++ routes/listing.js
@@ -1,3 +1,81 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const {listingSchema} = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Listing=require("../models/listing.js");
+
+
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+//ALL CRUD OPERATIONS BEGIN HERE
+
+//1.printing all of our data
+router.get("/", wrapAsync(async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+})
+)
+
+//3.NEW ROUTE - creating a new listing
+//CRUD equivalent - create operation
+router.get("/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+//2.Show Route- to show data of an individual listing
+//CRUD equivalent: read operation
+router.get("/:id" , wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs",{listing});
+})
+)
+
+//4.CREATE ROUTE - creating a new listing
+//CRUD equivalent - create operation
+router.post("/",validateListing,wrapAsync(async(req,res,next)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+    })
+)
+
+//5.EDIT ROUTE- edit existing listings
+//CRUD equivalent - update operation
+router.get("/:id/edit",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+})
+)
+
+//6.UPDATE ROUTE - to put the edited data into the database
+//CRUD equivalent- update operation
+router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+)
+
+//DELETE ROUTE - to delete a specific listing
+//CRUD equivalent - delete operation
+router.delete("/:id",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
+)
+
+module.exports = router;
 
