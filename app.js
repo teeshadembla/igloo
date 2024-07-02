bb26bad29d385771e751baabdd4149263b37aaa0
Teesha Dembla <teeshadembla0507@gmail.com>
2024-07-02 21:02:42 +0530

    last commit

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ if(process.env.NODE_ENV != "production"){
 
 const express = require("express");
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb://127.0.0.1:27017/igloo";
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
-
+const dbUrl = process.env.ATLASDB_URL;
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
@@ -24,7 +24,7 @@ const userRouter = require("./routes/user.js");
 const app = express(); 
 
 async function main(){
-    await  mongoose.connect(MONGO_URL );
+    await  mongoose.connect(dbUrl);
 }
 
 main().then(()=>{
@@ -40,8 +40,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24*3600,
+})
+
+store.on("error",()=>{
+    console.log("Error in mongo session store");
+})
+
 const sessionOptions = {
-    secret : "mysupersecretcode",
+    store,
+    secret : process.env.SECRET,
     resave : false,
     saveUninitialized: true,
     cookie: {
